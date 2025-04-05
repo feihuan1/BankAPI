22224be20d1791ac988946c9b39e322c499eaf3e
Feihuan Peng <feihuan.peng@spreetail.com>
2025-04-05 16:11:33 -0500

    Add project files.

---
+++ BankAPI/Controllers/BankController.cs
@@ -0,0 +1,58 @@
+﻿using BankAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Principal;
+
+namespace BankAPI.Controllers
+{
+    public class BankController : Controller
+    {
+
+        private readonly Account account = new Account { AccountNumber = 1001, AccountHolderName = "Eric", CurrentBalance = 5000 };
+
+        [Route("/")]
+        public IActionResult Index()
+        {
+            return Ok("Welcome to the Best Bank");
+        }
+
+        [Route("account-details")]
+        public IActionResult GetAccountInfo()
+        {
+            return Json(account);
+        }
+
+        [Route("statement")]
+        public IActionResult GetStatemant()
+        {
+            return File("/sample.pdf", "application/pdf");
+        }
+
+        [Route("get-current-banlance/{accountNumber:int}")]
+        public IActionResult GetBanlance()
+        {
+            if (Request.RouteValues["accountNumber"] is string accountNumberValue)
+            {
+                if(int.TryParse(accountNumberValue, out int accountNumber))
+                {
+                    if(accountNumber == account.AccountNumber)
+                    {
+                        return Ok(account.CurrentBalance);
+                    }
+                    else
+                    {
+                        return NotFound("account does not exist");
+                    }
+                }
+                else
+                {
+                    return BadRequest("account number has to be a number");
+                }
+            }
+            else
+            {
+                return NotFound("Account Number should be supplied");
+            }
+        }
+
+    }
+}
---
+++ BankAPI/Models/Account.cs
@@ -0,0 +1,9 @@
+﻿namespace BankAPI.Models
+{
+    public class Account
+    {
+        public int AccountNumber { get; set; }
+        public string AccountHolderName { get; set; }
+        public int CurrentBalance { get; set; }
+    }
+}
